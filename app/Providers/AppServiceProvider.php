76078b7cf6d240083bc7af80ad686b89e33ab777
Navy Horng <horngnavy2401@gmail.com>
2026-06-23 11:40:54 +0700

    fix: Force HTTPS scheme in production environment for URL generation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,8 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if(app()->environment('production')) {
-            \Illuminate\Support\Facades\URL::forceScheme('https');
-        }
+        \URL::forceScheme('https');
     }
 }
