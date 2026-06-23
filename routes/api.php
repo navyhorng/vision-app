bd7ccc64bc18f10fe79ebefee7da5a4cc61b476e
Navy Horng <horngnavy2401@gmail.com>
2026-06-23 14:12:31 +0700

    feat: Add ping endpoint for health check in API routes

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Api\ProductScanController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
