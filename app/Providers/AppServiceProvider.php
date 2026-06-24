1a3d0fa528b0ee70de6c27974409ef72ac392c8b
Navy Horng <horngnavy2401@gmail.com>
2026-06-24 16:02:09 +0700

    fix: Remove unnecessary HTTPS server variable setting in AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,6 @@ class AppServiceProvider extends ServiceProvider
     {
         if($this->app->environment('production')) {
             URL::forceScheme('https');
-            request()->server->set('HTTPS', true);
         }
     }
 }
