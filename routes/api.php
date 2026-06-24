8287b5e5bb26c7269e00a5176c4d1e257afb3af1
Navy Horng <horngnavy2401@gmail.com>
2026-06-24 13:10:18 +0700

    fix: Refactor login method in AuthController to include email validation and return structured JSON response

--- routes/api.php
+++ routes/api.php
@@ -6,21 +6,8 @@ use App\Http\Controllers\Api\ProductScanController;
 
 
 Route::post('/register', [AuthController::class, 'register']);
-// Route::post('/login', [AuthController::class, 'login']);
-Route::post('/login', function () {
-    return response()->json([
-        'status' => 'ok'
-    ]);
-});
-Route::any('/debug-post', function () {
-    return [
-        'method' => request()->method(),
-        'input' => request()->all(),
-    ];
-});
-Route::get('/ping', function () {
-    return response()->json(['message' => 'pong']);
-});
+Route::post('/login', [AuthController::class, 'login']);
+
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
