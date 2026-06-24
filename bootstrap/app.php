744ad1a28fcd9aee381de6ecfa61e29955cc79d2
Navy Horng <horngnavy2401@gmail.com>
2026-06-24 16:13:53 +0700

    feat: Integrate trust proxies configuration directly in app bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,9 +14,18 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-       $middleware->api(prepend: [
+        $middleware->api(prepend: [
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ]);
+
+         $middleware->trustProxies(
+            at: '*',
+            headers: Request::HEADER_X_FORWARDED_FOR
+                | Request::HEADER_X_FORWARDED_HOST
+                | Request::HEADER_X_FORWARDED_PORT
+                | Request::HEADER_X_FORWARDED_PROTO
+                | Request::HEADER_X_FORWARDED_PREFIX
+        );
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         // $exceptions->shouldRenderJsonWhen(
