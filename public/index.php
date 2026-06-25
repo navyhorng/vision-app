b68d2fa61765f4cf934359f67ddacd6b7cbd26e7
Navy Horng <horngnavy2401@gmail.com>
2026-06-25 09:09:09 +0700

    fix: Correctly handle HTTPS requests in index.php

--- public/index.php
+++ public/index.php
@@ -10,8 +10,17 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+if (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+    $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https') {
+    $_SERVER['HTTPS'] = 'on';
+    $_SERVER['REQUEST_SCHEME'] = 'https';
+    $_SERVER['SERVER_PORT'] = 443;
+}
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
+$app->handleRequest(Request::capture());
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
